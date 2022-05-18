eb1f242068cfea816231a36e01c8c7a0a94ca8ef
Jaime Colman <mjackerjaime@gmail.com>
2022-05-18 14:47:45 -0400

    Added run all colors on terminal.

---
+++ Colors/runColors.ps1
@@ -0,0 +1,8 @@
+### print all color table
+$colors = [enum]::GetValues([System.ConsoleColor])
+Foreach ($bgcolor in $colors){
+	Foreach ($fgcolor in $colors) { 
+		Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine
+	}
+	Write-Host " on $bgcolor"
+}
