b2959e5a3b350d9bc26b53724840a5c64533a3eb
Jaime Colman <mjackerjaime@gmail.com>
2022-05-20 15:24:21 -0400

    Added custom display for branches - Git.

--- Scripts/promptCustom.ps1
+++ Scripts/promptCustom.ps1
@@ -1,6 +1,17 @@
 function prompt{ 
 	Write-Host ("PS ") -ForegroundColor Yellow -BackgroundColor DarkBlue -NoNewLine
 	Write-Host ($((Get-Location | Get-Item).name)) -ForegroundColor White -BackgroundColor DarkGreen -NoNewLine
+	
+	# git
+	if (Test-Path .git) {
+		Write-Host "-" -NoNewline -ForegroundColor "DarkGreen"
+		Write-BranchName
+		Write-Host "" -NoNewLine
+	}
+	else {
+		# we're not in a repo so don't bother displaying branch name/sha
+		# Write-Host "" -ForegroundColor "green" -NoNewLine
+	}
 	Write-Host (">") -NoNewLine
 	return " "
 }
