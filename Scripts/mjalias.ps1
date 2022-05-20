15ecd0180dd33ad5d80264b3562a5579273fb8d2
Jaime Colman <mjackerjaime@gmail.com>
2022-05-20 15:23:26 -0400

    Add ls from linux terminal.

--- Scripts/mjalias.ps1
+++ Scripts/mjalias.ps1
@@ -6,7 +6,33 @@ function Get-DirNames{
 	Get-ChildItem -Name
 }
 
+# from stackoverflow.com/questions/1287718
+function Write-BranchName () {
+	try {
+		$branch = git rev-parse --abbrev-ref HEAD
+
+		if ($branch -eq "HEAD") {
+			# we're probably in detached HEAD state, so print the SHA
+			$branch = git rev-parse --short HEAD
+			Write-Host " ($branch)" -ForegroundColor "red" -NoNewLine
+		}
+		else {
+			# we're on an actual branch, so print it
+			Write-Host " ($branch)" -ForegroundColor "blue" -NoNewLine
+		}
+	} catch {
+		# we'll end up here if we're in a newly initiated git repo
+		Write-Host " (no branches yet)" -ForegroundColor "yellow" -NoNewLine
+	}
+}
+
+# ls from linux
+function Get-LsKali () {
+	wsl ls --color=auto -hF $args
+}
+
 
 New-Alias -Name lf Get-ListFolders
 New-Alias -Name ln Get-DirNames
 New-Alias -Name im Import-Module 
+New-alias -Name lk -Value Get-LsKali -Option AllScope
